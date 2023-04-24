e9a1a112f797f83689142cd5b4cbae7c2766bd60
Bebe Jane <bjornthief@gmail.com>
2023-04-24 22:23:30 +0200

    dings

--- components/common/SelectedWorks.tsx
+++ components/common/SelectedWorks.tsx
@@ -1,6 +1,6 @@
 import s from './SelectedWorks.module.scss'
 import cn from 'classnames'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { Image } from 'react-datocms'
 
 type Props = {
@@ -12,26 +12,35 @@ export default function SelectedWorks({ artwork }: Props) {
   const [show, setShow] = useState(false)
   const [fade, setFade] = useState(false)
   const [index, setIndex] = useState(0)
+  const [nextTopMargin, setNextTopMargin] = useState<number | undefined>()
+  const timeoutRef = useRef<NodeJS.Timer | null>()
+  const currentArtwork = artwork[index]
+
   const style = { transform: `translateX(-${index * 100}%)` }
+
+  const handleMouseMove = ({ clientY }: React.MouseEvent) => {
+    setNextTopMargin(clientY)
+    clearTimeout(timeoutRef.current)
+    timeoutRef.current = setTimeout(() => setNextTopMargin(undefined), 1000)
+  }
+
   const handleClick = () => {
-
     setFade(true)
-    setTimeout(() => {
-      setIndex(index === artwork.length - 1 ? 0 : index + 1)
-    }, 400)
-
-    setTimeout(() => {
-      setFade(false)
-    }, 700)
-
+    setTimeout(() => setIndex(index === artwork.length - 1 ? 0 : index + 1), 400)
+    setTimeout(() => setFade(false), 700)
   }
 
   useEffect(() => {
     setTimeout(() => setShow(true), 2000)
   }, [])
 
+
   return (
-    <div className={s.container}>
+    <div
+      className={cn(s.container, s[currentArtwork.layout])}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={() => setNextTopMargin(undefined)}
+    >
       <h2 className={cn(show && s.hide)}>
         SELECTED<br />
         WORK<br />
@@ -55,6 +64,7 @@ export default function SelectedWorks({ artwork }: Props) {
         <div className={s.left}></div>
         <div className={s.right}></div>
       </div>
+      {nextTopMargin && show && <nav className={s.next} style={{ top: `${nextTopMargin}px` }}>Next</nav>}
     </div>
   )
 }
